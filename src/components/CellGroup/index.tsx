7dffca623b3f84daff0ed803773e30932545d243
doll <lslabyrinth@163.com>
2021-08-27 11:13:55 +0800

    pulish

--- src/components/CellGroup/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./CellGroup.css";
-
-const CellGroup: React.FC = props => {
-  return <div className="cell-group">{props.children}</div>;
-};
-
-export default CellGroup;
